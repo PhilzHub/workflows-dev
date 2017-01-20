924aa1bdd32cb8caf575e1ad67f8026d8643436f
Brian Zoro <bzbzhdi@gmail.com>
2017-01-20 10:10:51 -0500

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 // gulp.task('log', function() {
 //   gutil.log('Workflows are awesome');
@@ -8,6 +9,13 @@ var gulp = require('gulp'),
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 // var coffeeSources = ['components/coffee/*.coffee'];  // any coffee files
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js',
+];
+
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -15,4 +23,10 @@ gulp.task('coffee', function() {
     .pipe(coffee({ bare: true })
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
